af1ff056e661eebf143a069bd338e1536a993fb4
Kabin_giri <85396529+Kabin119245@users.noreply.github.com>
2024-06-06 20:43:39 +0545

    tailwind css added

--- scm2.0/src/main/java/com/scm/controllers/PageController.java
+++ scm2.0/src/main/java/com/scm/controllers/PageController.java
@@ -1,14 +1,17 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping(value = "/home")
-    public String home() {
+    public String home(Model model) {
         System.out.println("Home Page handler");
+        model.addAttribute("name", "Hello World");
+        model.addAttribute("title", "Hello World");
         return "home";
     }
 
