eaf300e655ea30aa35b37e10f955dee7a563d62d
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-06-19 22:44:02 +0545

    UserDetails implemented

--- scm2.0/src/main/java/com/scm/config/SecurityConfig.java
+++ scm2.0/src/main/java/com/scm/config/SecurityConfig.java
@@ -1,6 +1,11 @@
 package com.scm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
@@ -27,8 +32,25 @@ public class SecurityConfig {
 //        return inMemoryUserDetailsManager;
 //
 //    }
-    
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+       DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+       //object of user detail service
+       daoAuthenticationProvider.setUserDetailsService();
+
+       //object of password encoder
+       daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
+    }
 
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- scm2.0/src/main/java/com/scm/entities/User.java
+++ scm2.0/src/main/java/com/scm/entities/User.java
@@ -3,8 +3,11 @@ package com.scm.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity(name = "user")
@@ -14,13 +17,14 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
     private String userId;
     @Column(name = "user_name", nullable = false)
     private String name;
     @Column(unique = true, nullable = false)
     private String email;
+    @Getter(AccessLevel.NONE)
     private String password;
     @Column(length = 10000)
     private String about;
@@ -29,7 +33,8 @@ public class User {
     private String phoneNumber;
 
     //information
-    private boolean enabled = false;
+    @Getter(AccessLevel.NONE)
+    private boolean enabled = true;
     private boolean emailVerified = false;
     private boolean phoneVerified = false;
 
@@ -45,7 +50,43 @@ public class User {
     @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY,orphanRemoval = true)
     private List<Contact> contacts = new ArrayList<>();
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roleList = new ArrayList<>();
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    @Override
+    public String getPassword() {
+
+        return this.password;
+    }
 
 }
