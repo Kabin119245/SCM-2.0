6881b65c7f41ca842c303ed2a47b6507508515b5
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-06-22 21:52:04 +0545

    spring security for login completed

--- scm2.0/src/main/java/com/scm/config/SecurityConfig.java
+++ scm2.0/src/main/java/com/scm/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.scm.services.impl.SecurityCustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- scm2.0/src/main/java/com/scm/controllers/PageController.java
+++ scm2.0/src/main/java/com/scm/controllers/PageController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,13 @@ public class PageController {
 
     @Autowired
     private UserService userService;
+
+
+    @GetMapping(value = "/")
+    public String index() {
+
+        return "redirect:/home";
+    }
 
     @RequestMapping(value = "/home")
     public String home(Model model) {
