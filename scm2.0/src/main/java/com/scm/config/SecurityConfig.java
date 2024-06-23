d258a5a760c0c38bff30f0fd429f33e9bdb1e198
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-06-23 23:53:22 +0545

    login/out backend

--- scm2.0/src/main/java/com/scm/config/SecurityConfig.java
+++ scm2.0/src/main/java/com/scm/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -88,6 +89,12 @@ public class SecurityConfig {
                     .failureForwardUrl("/login?error=true")
                     .usernameParameter("email")
                     .passwordParameter("password");
+        });
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.logout(logoutForm -> {
+            logoutForm.logoutUrl("/logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
         });
 
         return httpSecurity.build();
